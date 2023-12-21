37703a392cc8abd713499f9a6232449381e59390
r8bywork <vanya.smolensky@gmail.com>
2023-12-22 00:06:47 +0300

    feat: i wanna sleep

--- src/App.tsx
+++ src/App.tsx
@@ -31,21 +31,128 @@ const App = () => {
         </div>
       </div>
 
-      {/* <div className='bg-gradient-to-b from-transparent via-black/100 to-black/100'>*/}
+      {/*<div className='bg-gradient-to-b from-transparent via-black/100 to-black/100'>*/}
       <div className={'bg-black pt-[30px]'}>
         <div className={'w-[1080px] mx-auto'}>
           <div
-            className='w-full mx-auto  text-lg text-white mb-[20px]'
+            className='w-full mx-auto text-lg text-white mb-[20px] indent-[20px]'
             id={'wallet'}
           >
             <h2 className='text-4xl font-bold mb-4'>Wallet</h2>
             <p>Nintondo Wallet for Bells - A New Horizon for Your Crypto Adventures!</p>
+            <p>🌿 Version 0.0.4 - Bringing the Charm of Animal Crossing to Cryptocurrency</p>
+            <p>🍃 Welcome to Nintondo Wallet! 🍃</p>
             <p>
               We're thrilled to introduce Nintondo Wallet, your newest companion in the whimsical
               world of Bells cryptocurrency. Inspired by the beloved universe of Animal Crossing,
               our wallet is more than just a tool; it's a delightful journey into the heart of a
               community where fun meets finance.
             </p>
+            <br />
+            <p>🌸 Key Features:</p>
+            <ul className={'list-disc'}>
+              <li>
+                <p>
+                  Village Marketplace: Just like your favorite Animal Crossing marketplace, trade
+                  and manage your Bells with ease and charm.
+                </p>
+              </li>
+              <li>
+                <p>
+                  Tom Nook's Security: Top-notch security measures, ensuring your Bells is as safe
+                  as a bell in Tom Nook's vault.
+                </p>
+              </li>
+              <li>
+                <p>
+                  Island Backup: Never lose your data with our Island Backup system, safeguarding
+                  your wallet like the serene islands of Animal Crossing.
+                </p>
+              </li>
+              <li>
+                <p>
+                  Nook Miles Rewards: Earn Nook Miles for every transaction, adding an exciting
+                  twist to your cryptocurrency journey.
+                </p>
+              </li>
+              <li>
+                <p>
+                  Customizable Interface: Personalize your wallet with themes and characters from
+                  Animal Crossing, making finance fun!
+                </p>
+              </li>
+            </ul>
+            <br />
+            <p>🍂 What's New in 0.0.4:</p>
+            <ul className={'list-disc'}>
+              <li>
+                <p>
+                  Launch of Nintondo Wallet: A fresh start with a familiar feel for all Bells
+                  enthusiasts and Animal Crossing fans.
+                </p>
+              </li>
+              <li>
+                <p>
+                  Enhanced UI: Navigate with ease through a user-friendly interface, adorned with
+                  charming Animal Crossing aesthetics.
+                </p>
+              </li>
+              <li>
+                <p>
+                  Improved Transaction Speed: Swift as a balloon gift floating across the sky, our
+                  enhanced transaction system ensures quick and efficient processing.
+                </p>
+              </li>
+              <li>
+                <p>
+                  Community Events: Participate in special events and challenges, bringing together
+                  the community spirit of Animal Crossing.
+                </p>
+              </li>
+            </ul>
+            <br />
+            <p>🌟 Join Our Community:</p>
+            <p>
+              Step into a world where your financial journey is intertwined with the charm and
+              simplicity of Animal Crossing. Join our community, share tips, and make new friends,
+              all while managing your Bells cryptocurrency. Let's create a community as heartwarming
+              and supportive as the townsfolk of Animal Crossing!
+            </p>
+            {/*list-disc pl-4*/}
+            <br />
+            <p className={'font-bold'}>📥 Download Now:</p> <br />
+            <p>
+              <a
+                className={'underline'}
+                href='https://github.com/Nintondo/extension/releases/download/0.0.7/chrome-0.0.7.zip'
+              >
+                Chrome Extension
+              </a>
+            </p>
+            <br /> <br />
+            <p>
+              <a
+                className={'underline'}
+                href={
+                  'https://github.com/Nintondo/extension/releases/download/0.0.7/firefox-0.0.7.xpi'
+                }
+              >
+                Firefox Extension
+              </a>
+            </p>
+            <br /> <br />
+            <p>
+              Ready to embark on this enchanting crypto adventure? Download Nintondo Wallet for
+              Bells and turn your cryptocurrency experience into an idyllic escapade. Let's make our
+              financial journey not just profitable, but also delightful!
+            </p>
+            <p>
+              Note: Nintondo Wallet is not affiliated with Nintendo or the Animal Crossing
+              franchise. Bells is a meme cryptocurrency and should be enjoyed as part of a balanced
+              financial portfolio.
+            </p>
+            <br />
+            <p>Happy Bell Hunting! 🛎️</p>
           </div>
 
           <div
@@ -63,7 +170,7 @@ const App = () => {
                       <li key={linkIndex}>
                         <a
                           href={link.link}
-                          target={'_blank'}
+                          target='_blank'
                           className='underline'
                         >
                           {link.name}
