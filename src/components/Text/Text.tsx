7059925554f23d8495836c08461e95591265c9a6
r8bywork <vanya.smolensky@gmail.com>
2023-12-26 11:45:11 +0300

    feat: add adaptive cursor

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,10 +1,12 @@
 const Text = () => {
   return (
     <div className={'text-center text-white flex flex-col items-center'}>
-      <div className='mb-8 text-[40px] px-[15px] relative'>
-        <span className={'p-[5px] bg-black'}>Cryptocurrency based on the game </span>
-        <span className={'p-[5px] bg-black text-regal-yellow'}>Animal Crossing</span>
-        {/*<div className='absolute right-0 bottom-0 w-4 h-14 bg-yellow-500'></div>*/}
+      <div className='mb-8 text-[40px] md:justify-center relative'>
+        <span className={'pl-[10px] bg-black'}>Cryptocurrency based on the game </span>
+        <span className={'pr-[20px] bg-black text-regal-yellow relative'}>
+          Animal Crossing
+          <div className='absolute right-0 bottom-0 w-3 h-11 bg-yellow-500'></div>
+        </span>
       </div>
 
       <div className='text-[20px] md:flex md:justify-center md:flex-col items-center'>
@@ -14,7 +16,7 @@ const Text = () => {
           <span className='text-regal-blue'> exchange</span> them for other
         </div>
         <div className='inline bg-black px-[5px]'>
-          <span className={''}>
+          <span>
             cryptocurrencies and track them with <span className='text-regal-blue'>explorer</span>.
           </span>
         </div>
