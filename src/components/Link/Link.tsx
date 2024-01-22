62130896f91c5ee365294d6de90e70fba201b0cd
r8bywork <vanya.smolensky@gmail.com>
2024-01-22 13:08:00 +0300

    feat: change links logic in header

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom';
+
 interface LinkProps {
   href: string;
   text: string;
@@ -6,11 +8,27 @@ interface LinkProps {
 }
 
 const Link = ({ href, text, className, onClick }: LinkProps) => {
+  if (href.startsWith('/')) {
+    return (
+      <NavLink
+        to={href}
+        onClick={onClick}
+        className={`text-base font-bold leading-normal hover:text-yellow-500 transition-all duration-300 ease-in-out hover:underline ${className}`}
+        style={{
+          textDecorationColor: '#FBBF24',
+          textDecorationThickness: '2px',
+          textUnderlineOffset: '8px',
+        }}
+      >
+        {text.toUpperCase()}
+      </NavLink>
+    );
+  }
+
   return (
     <a
-      // to={href}
       href={href}
-      target={href.startsWith('#') ? '_self' : '_target'}
+      target={href[2] === '#' ? '_self' : '_target'}
       onClick={onClick}
       className={`text-base font-bold leading-normal hover:text-yellow-500 transition-all duration-300 ease-in-out hover:underline ${className}`}
       style={{
